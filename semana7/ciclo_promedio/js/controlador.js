5a5902aba3f24b4d15c3ebf2c6a814f6c0be4ac7
pabskun <pablo.monestel@gmail.com>
2018-06-19 18:58:37 -0600

    add
    Add: semana 7

---
+++ semana7/ciclo_promedio/js/controlador.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let botonCalcular = document.querySelector('#btnCalcular');
+let inputCantNumeros = document.querySelector('#txtCantNumeros');
+let outputPromedio =  document.querySelector('#txtPromedio');
+
+botonCalcular.addEventListener('click', obtenerDatos);
+
+function obtenerDatos(){
+    let nCantNumeros = 0;
+    let nNumero = 0;
+    let nSumatoria = 0;
+    let nPromedio = 0;
+
+    nCantNumeros = Number(inputCantNumeros.value);
+
+    //Ciclo incondicional
+    for(let i = 0; i < nCantNumeros; i++){
+        //cuerpo del ciclo
+       nNumero = Number(prompt('Ingrese el número'));
+       nSumatoria  = determinarSumatoria(nSumatoria , nNumero);
+    }
+    nPromedio = determinarPromedio(nSumatoria, nCantNumeros);
+    outputPromedio.value = nPromedio;
+};
