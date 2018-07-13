277674f0935a395a0df015babed1cccfe0e83d2f
pabskun <pablo.monestel@gmail.com>
2018-07-12 18:31:20 -0600

    commit semana 10 / vecindad

---
+++ semana10/vecindad/public/js/vecinos/servicio_vecinos.js
@@ -0,0 +1,25 @@
+'use strict';
+const mVecinos = [
+    ['Concha', 64, 'femenino', 1],
+    ['Agata', 25, 'femenino', 1],
+    ['Amandine', 46, 'femenino', 2],
+    ['Porfirio', 15, 'masculino', 2],
+    ['Celestino', 62, 'masculino', 2],
+    ['Morgana', 4, 'femenino', 3],
+    ['Maury', 34, 'masculino', 3],
+    ['Leo', 37, 'masculino', 3],
+    ['Constanza', 7, 'femenino', 4],
+    ['Consuelo', 29, 'femenino', 4],
+    ['Hermenegildo', 56, 'masculino', 5],
+    ['Laudencio', 78, 'masculino', 6],
+    ['Petruzca', 66, 'femenino', 6],
+    ['Teodolfo', 54, 'masculino', 6],
+    ['Héctor', 41, 'masculino', 7],
+    ['Andrey Tercero', 17, 'masculino', 7],
+    ['Aurelia', 39, 'femenino', 7],
+    ['Reflejo', 21, 'femenino', 7]
+];
+
+
+    
+
