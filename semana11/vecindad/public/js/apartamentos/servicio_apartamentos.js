1855d94565934c613593d61fa05a9f60dc960e93
pabskun <pablo.monestel@gmail.com>
2018-07-24 19:06:10 -0600

    commit

--- semana11/vecindad/public/js/apartamentos/servicio_apartamentos.js
+++ semana11/vecindad/public/js/apartamentos/servicio_apartamentos.js
@@ -1,20 +1,37 @@
 'use strict';
-const mApartamento = [
-    [1, 'Apartamento rojo', 100, 2],
-    [2, 'Apartamento verde', 120, 3],
-    [3, 'Apartamento negro', 160, 3],
-    [4, 'Apartamento amarillo', 90, 2],
-    [5, 'Apartamento azul', 75, 1],
-    [6, 'Apartamento blanco', 107, 3],
-    [7, 'Apartamento tornasol', 185, 4]
-];
+// const mApartamento = [
+//     [1, 'Apartamento rojo', 100, 2],
+//     [2, 'Apartamento verde', 120, 3],
+//     [3, 'Apartamento negro', 160, 3],
+//     [4, 'Apartamento amarillo', 90, 2],
+//     [5, 'Apartamento azul', 75, 1],
+//     [6, 'Apartamento blanco', 107, 3],
+//     [7, 'Apartamento tornasol', 185, 4]
+// ];
 
 function registrarApartamento(pNuevoApartamento){
-    mApartamento.push(pNuevoApartamento);
+    let mApartamentos = obtenerListaApartamentos();
+    mApartamentos.push(pNuevoApartamento);
+
+    localStorage.setItem('mApartamentosLS', JSON.stringify(mApartamentos));
 };
 
 function obtenerListaApartamentos(){
-    return mApartamento;
+    let mApartamentos = JSON.parse(localStorage.getItem('mApartamentosLS'));
+
+    if(mApartamentos == null){
+        mApartamentos = [
+            [1, 'Apartamento rojo', 100, 2],
+            [2, 'Apartamento verde', 120, 3],
+            [3, 'Apartamento negro', 160, 3],
+            [4, 'Apartamento amarillo', 90, 2],
+            [5, 'Apartamento azul', 75, 1],
+            [6, 'Apartamento blanco', 107, 3],
+            [7, 'Apartamento tornasol', 185, 4]
+        ];
+    }
+    
+    return mApartamentos;
 };
 function obtenerCantidadInquilinos(pId){
     let listaApartamentos = [];
